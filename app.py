f40344800371d94fd146a982ae45b32d81b6231d
Aviral <aviral.jain2020@vitstudent.ac.in>
2022-01-05 21:06:04 +0530

    deployment files

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
-from flask import Flask, render_template, Response
+from sys import stdout
+from process import webopencv
+import logging
+from flask import Flask, render_template, Response, request, jsonify
+from flask_socketio import SocketIO
+from camera import Camera
+from utils import base64_to_pil_image, pil_image_to_base64
 import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
+#----------------- Video Transmission ------------------------------#
+app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+camera = Camera(webopencv())
 
-def gen_frames():  # generate frame by frame from camera
+#---------------- Video Transmission --------------------------------#
+
+#---------------- Video Socket Connections --------------------------#
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input = input.split(",")[1]
+    camera.enqueue_input(input)
+    #camera.enqueue_input(base64_to_pil_image(input))
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    app.logger.info("client connected")
+
+
+def gen():
+    """Video streaming generator function."""
+
+    app.logger.info("starting to generate frames!")
     while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
+        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
@@ -33,4 +53,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
